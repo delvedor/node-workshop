eb5ea85cd5725cea43511e50f486350e04d10404
delvedor <tommydelved@gmail.com>
2016-06-07 16:59:45 +0200

    Updated test

--- test.js
+++ test.js
@@ -1,15 +1,29 @@
 'use strict'
 
 const test = require('tape')
-const point = require('./point')('test')
 const mongojs = require('mongojs')
 const db = mongojs('test', ['timeseries'])
 const mongoClean = require('mongo-clean')
+// Clean the db
 mongoClean('mongodb://localhost:27017/test', function () {})
+
+const point = require('./point')('test')
 
 test('insert', function (t) {
   t.plan(6)
   t.is(typeof point.insert, 'function')
+  // test the validator
+  point.insert({
+    category: 42,
+    time: new Date(),
+    data: {}
+  }, function callback (err, value) {
+    if (err) {
+      t.pass('validator ok!')
+    } else {
+      t.fail('validator fails')
+    }
+  })
   // Correct insert
   let obj = {
     category: 'prova',
@@ -23,18 +37,6 @@ test('insert', function (t) {
       t.pass('ok!')
     }
   })
-  // Wrong insert
-  point.insert({
-    category: 42,
-    time: new Date(),
-    data: {}
-  }, function callback (err, value) {
-    if (err) {
-      t.pass('ok!')
-    } else {
-      t.fail('insert error 2')
-    }
-  })
   // Test the insert
   db.timeseries.findOne(function (err, value) {
     if (err) t.fail('find fails')
@@ -45,25 +47,34 @@ test('insert', function (t) {
 })
 
 test('fetch', function (t) {
-  t.plan(3)
+  t.plan(4)
   t.is(typeof point.fetch, 'function')
+  // Test the validator
+  point.fetch({}, function callback (err, result) {
+    err ? t.pass('validator ok!') : t.fail('vailidator fails')
+  })
+  // Object to insert
   let obj = {
     category: 'prova',
     time: new Date(),
     data: {}
   }
+  // Object to find
   let find = {
     category: 'prova',
     from: obj.time,
     to: obj.time
   }
+  // Falsey result
   let findFalse = {
     category: 'nope',
     from: new Date(),
     to: new Date()
   }
+  // Insert the object
   point.insert(obj, function callback (err, value) {
     if (err) console.log(err)
+    // test fetch
     point.fetch(find, function (err1, documents) {
       if (err1) {
         t.fail('fetch fails')
@@ -72,6 +83,7 @@ test('fetch', function (t) {
       }
     })
   })
+  // test falsey
   point.fetch(findFalse, function (err1, documents) {
     if (err1) {
       t.fail('fetch fail')
@@ -81,6 +93,7 @@ test('fetch', function (t) {
   })
 })
 
+// Clean the db every time
 test.onFinish(function () {
   mongoClean('mongodb://localhost:27017/test', function () {})
 })
