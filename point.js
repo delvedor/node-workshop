b6b6f504e1e20f69cc46152cd6e5bd21d6ae2acf
delvedor <tommydelved@gmail.com>
2016-06-07 15:31:30 +0200

    First implementation insert

--- point.js
+++ point.js
@@ -1,8 +1,38 @@
 'use strict'
+const mongojs = require('mongojs')
+const Joi = require('Joi')
 
-function insert (obj) {}
+const db = mongojs('test', ['timeseries'])
+const insertVal = Joi.object().keys({
+  category: Joi.string().required(),
+  time: Joi.date().required(),
+  data: Joi.object().required()
+})
+const fetchVal = Joi.object().keys({
+  category: Joi.string().required(),
+  from: Joi.date().required(),
+  to: Joi.date().required()
+})
 
-function fetch (obj) {}
+// Insert a key
+function insert (obj, callback) {
+  Joi.validate(obj, insertVal, function (err, value) {
+    if (err) {
+      callback(err, null)
+    } else {
+      db.timeseries.insert(obj)
+      callback(null, 'ok!')
+    }
+  })
+}
+
+// Fetch keys
+function fetch (obj, callback) {
+  Joi.vaidate(obj, fetchVal, function (err, value) {
+    if (err) console.log(err)
+    console.log(value)
+  })
+}
 
 module.exports.insert = insert
 module.exports.fetch = fetch
--- test.js
+++ test.js
@@ -1,1 +1,41 @@
+'use strict'
+
+const test = require('tape')
+const point = require('./point')
+// const mongojs = require('mongojs')
+// const db = mongojs('test', ['timeseries'])
+
+test('insert', function (t) {
+  t.plan(3)
+  t.is(typeof point.insert, 'function')
+  // Correct insert
+  point.insert({
+    category: 'prova',
+    time: new Date(),
+    data: {}
+  }, function callback (err, value) {
+    if (err) {
+      t.fail('insert error 1')
+    } else {
+      t.pass('ok!')
+    }
+  })
+  // Wrong insert
+  point.insert({
+    category: 42,
+    time: new Date(),
+    data: {}
+  }, function callback (err, value) {
+    if (err) {
+      t.pass('ok!')
+    } else {
+      t.fail('insert error 2')
+    }
+  })
+})
+
+test('fetch', function (t) {
+  t.plan(1)
+  t.is(typeof point.fetch, 'function')
+})
 
